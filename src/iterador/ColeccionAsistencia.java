66898ee2721611fee18096a2c81dab9be671fee5
Piero <pier0ocj77@gmail.com>
2026-06-13 21:38:58 -0500

    reformat: corregir imports y método createIterator en ColeccionAsistencia,

--- src/iterador/ColeccionAsistencia.java
+++ src/iterador/ColeccionAsistencia.java
@@ -1,26 +1,27 @@
-    package iterador;
+package iterador;
 
-    import Interface.Iterador;
-    import model.RegistroAsistencia;
+import Interface.Iterable;
+import Interface.Iterador;
+import model.RegistroAsistencia;
 
-    public class ColeccionAsistencia {
+public class ColeccionAsistencia implements Iterable {
 
-        private RegistroAsistencia[] registros;
-        private int indice = 0;
+    private RegistroAsistencia[] registros;
+    private int indice = 0;
 
-        public ColeccionAsistencia() {
-            registros = new RegistroAsistencia[10];
-        }
+    public ColeccionAsistencia() {
+        registros = new RegistroAsistencia[10];
+    }
 
-        public void agregarRegistro(RegistroAsistencia registro) {
-            if (indice < registros.length) {
-                registros[indice] = registro;
-                indice++;
-            }
-        }
-
-        @Override
-        public Iterador createIterator() {
-            return new ArrayIterator(registros);
+    public void agregarRegistro(RegistroAsistencia registro) {
+        if (indice < registros.length) {
+            registros[indice] = registro;
+            indice++;
         }
     }
+
+    @Override
+    public Iterador crearIterador() {
+        return new ArrayIterator(registros);
+    }
+}
